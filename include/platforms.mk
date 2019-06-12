e7eee788ac421dfb42156b0fc768d4b5121d03c5
Mike Neish <Michael.Neish@canada.ca>
2019-06-12 14:59:00 -0400

    Roll EXTRA_LINKS into FFLAGS for simplification, and allow SHAREDLIB_SUFFIX to be overridden from the calling environment.

--- include/platforms.mk
+++ include/platforms.mk
@@ -8,33 +8,32 @@ RPN_MACRO_DIR = $(PROJECT_ROOT)/include
 ifeq ($(PLATFORM),manylinux1_x86_64)
   CFLAGS := $(CFLAGS) -m64
   FFLAGS := $(FFLAGS) -m64
-  SHAREDLIB_SUFFIX = so
 else ifeq ($(PLATFORM),manylinux1_i686)
   CFLAGS := $(CFLAGS) -m32
   FFLAGS := $(FFLAGS) -m32
-  SHAREDLIB_SUFFIX = so
 else ifeq ($(PLATFORM),manylinux2010_x86_64)
   CFLAGS := $(CFLAGS) -m64
   FFLAGS := $(FFLAGS) -m64
-  SHAREDLIB_SUFFIX = so
 else ifeq ($(PLATFORM),win_amd64)
   CC = x86_64-w64-mingw32-gcc
   FC = x86_64-w64-mingw32-gfortran
-  EXTRA_LINKS := -lws2_32 -lpthread
+  FFLAGS := $(FFLAGS) -lws2_32 -lpthread
   SHAREDLIB_SUFFIX = dll
 else ifeq ($(PLATFORM),win32)
   CC = i686-w64-mingw32-gcc
   FC = i686-w64-mingw32-gfortran
-  EXTRA_LINKS := -lws2_32 -lpthread
+  FFLAGS := $(FFLAGS) -lws2_32 -lpthread
   SHAREDLIB_SUFFIX = dll
-# Default - assume building on a Linux machine.
-else
-  SHAREDLIB_SUFFIX = so
 endif
 
 # Set default fortran compiler to gfortran.
 ifeq ($(origin FC),default)
 FC = gfortran
+endif
+ifneq (,$(findstring gfortran,$(FC)))
 FFLAGS := $(FFLAGS) -fcray-pointer -ffree-line-length-none
 endif
+
+# Set default shared library extension for Linux.
+SHAREDLIB_SUFFIX ?= so
 
