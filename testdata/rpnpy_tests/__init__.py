ec4bb3d25d202922f02ec155167182d793955a14
Mike Neish <Michael.Neish@canada.ca>
2019-07-08 13:28:34 -0400

    Force the test data to be read-only, since apparently some of the tests try to open them in write mode and expect to fail.

--- testdata/rpnpy_tests/__init__.py
+++ testdata/rpnpy_tests/__init__.py
@@ -6,24 +6,34 @@ def _setenv (varname, value):
 
 def testenv():
   import rpnpy_tests
-  from os.path import join, dirname
-  from os import makedirs
+  from os.path import join, dirname, exists
+  from os import makedirs, walk, chmod
+  import stat
   from tempfile import mkdtemp
-  from shutil import copy
+  from shutil import copy, copytree
   from datetime import datetime
-  root = dirname(rpnpy_tests.__file__)
-  for varname in ('ATM_MODEL_DFILES','AFSISIO','rpnpy'):
-    value = join(root,varname)
-    _setenv (varname, value)
+  orig = dirname(rpnpy_tests.__file__)
   tmpdir = mkdtemp()
   _setenv ('TMPDIR', tmpdir)
-  makedirs(join(tmpdir,'prog','regeta'), exist_ok=True)
-  copy( join(root,'CMCGRIDF','prog','regeta','2019033000_048'),
-        join(tmpdir,'prog','regeta',datetime.today().strftime('%Y%m%d00_048'))
+  for varname in ('ATM_MODEL_DFILES','AFSISIO','rpnpy'):
+    src = join(orig,varname)
+    dst = join(tmpdir,varname)
+    copytree (src,dst)
+    _setenv (varname,dst)
+    for root, dirs, files in walk(dst):
+      for x in dirs+files:
+        try:
+          chmod(join(root,x),0o555)
+        except PermissionError:
+          pass
+  makedirs(join(tmpdir,'CMCGRIDF','prog','regeta'))
+  copy( join(orig,'CMCGRIDF','prog','regeta','2019033000_048'),
+        join(tmpdir,'CMCGRIDF','prog','regeta',datetime.today().strftime('%Y%m%d00_048'))
   )
-  _setenv ('CMCGRIDF',tmpdir)
+  _setenv ('CMCGRIDF',join(tmpdir,'CMCGRIDF'))
   _setenv ('RPNPY_NOLONGTEST','1')
-  makedirs ('tmp', exist_ok=True)
+  if not exists('tmp'):
+    makedirs ('tmp')
 
 def test():
   import pytest
