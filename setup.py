24cd7d114c8280f8260aefc9ebd4d8f10a0e9c50
Mike Neish <Michael.Neish@canada.ca>
2020-03-10 11:17:36 -0400

    Fix reStructuredText format for long description.

--- setup.py
+++ setup.py
@@ -34,6 +34,7 @@ setup (
   description = 'Dependencies for the fstd2nc package.',
   long_description = """
 Provides the Python-RPN_ package, along with the libraries_ needed for fstd2nc.
+
 .. _Python-RPN: https://github.com/meteokid/python-rpn
 .. _libraries: https://github.com/neishm/python-rpn-libsrc
 """,
